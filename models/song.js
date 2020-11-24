fc33d0919f82dbb72b3f5a807766f174e25037c6
giocode <giocordova@gmail.com>
2020-11-24 15:03:56 -0500

    added song_artist to song.js and updated playlist_content column to use ms 'song_start_ms'

--- models/song.js
+++ models/song.js
@@ -7,6 +7,13 @@ module.exports = function(sequelize, DataTypes) {
           len: [1]
         }
       },
+      song_artist: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
       genre: {
         type: DataTypes.INT
       }
