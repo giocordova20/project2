d05b13c5b9bff1710ec004874fced7192b670a21
jaguar6105 <gilljoseph603@gmail.com>
2020-12-03 23:24:48 -0500

    added spotify api calls

--- routes/spotify_api.js
+++ routes/spotify_api.js
@@ -5,11 +5,11 @@ var qs = require('querystring');
 var SpotifyWebApi = require('spotify-web-api-node');
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : '7d508c40819643bd9043057da1035acc',
-  clientSecret : '3438a5114e1b468ba7a8ace70662d829',
+  clientId: '7d508c40819643bd9043057da1035acc',
+  clientSecret: '3438a5114e1b468ba7a8ace70662d829',
 });
 
-const jssdkscopes = ["streaming", "user-read-birthdate", "user-read-email", "user-read-private", "user-modify-playback-state"];
+const jssdkscopes = ["streaming", "user-read-birthdate", "user-read-email", "user-read-private", "user-modify-playback-state", "playlist-modify-private", "playlist-modify-public"];
 const redirectUriParameters = {
   client_id: '7d508c40819643bd9043057da1035acc',
   response_type: 'token',
@@ -22,86 +22,88 @@ const redirectUri = `https://accounts.spotify.com/authorize?${qs.stringify(redir
 
 function authenticate(callback) {
   spotifyApi.clientCredentialsGrant()
-    .then(function(data) {
+    .then(function (data) {
       console.log('The access token expires in ' + data.body['expires_in']);
       console.log('The access token is ' + data.body['access_token']);
-    
+
       callback instanceof Function && callback();
 
       // Save the access token so that it's used in future calls
       spotifyApi.setAccessToken(data.body['access_token']);
-    }, function(err) {
+    }, function (err) {
       console.log('Something went wrong when retrieving an access token', err.message);
     });
 }
 authenticate();
 
-module.exports = function(app, err) {
+module.exports = function (app, err) {
 
   if (err) throw err;
-  app.get("/playlists/:pl", function (req,res) {
+  app.get("/playlists/:pl", function (req, res) {
 
     spotifyApi.getPlaylist(req.params.pl) //'34inxLjFfBchpVk3nPQuDf')
-    .then(function(data){
+      .then(function (data) {
 
-      let tracks = data.body.tracks.items;
+        let tracks = data.body.tracks.items;
 
-      // console.log("    --------------------------------\n    data: ",data, "  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n\n\n\n\n\n\n\n");
-      // console.log("     >>> \n     tracks: ", tracks)
-      
-      // console.log("     \n\n\n\n\n\n\n\n\n\<<<<<<<<<<  >>>>>>>>>>>");
-      // console.log("    \n\n\n *** tracks[1] *** ", tracks[1]);
-      // console.log("    \n\n\n *** tracks[1].track.album.images[0] *** ", tracks[1].track.album.images);
-      // console.log("    \n\n\n *** tracks[1].track.album.images[0] *** ", tracks[1].track.album.images[2]);
-      // console.log("    \n\n\n    *** tracks[1].track.name *** :         ",tracks[1].track.name);
-      // console.log("    *** tracks[1].track.artists.name *** : ", tracks[1].track.artists[0].name);
+        // console.log("    --------------------------------\n    data: ",data, "  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n\n\n\n\n\n\n\n");
+        // console.log("     >>> \n     tracks: ", tracks)
+
+        // console.log("     \n\n\n\n\n\n\n\n\n\<<<<<<<<<<  >>>>>>>>>>>");
+        // console.log("    \n\n\n *** tracks[1] *** ", tracks[1]);
+        // console.log("    \n\n\n *** tracks[1].track.album.images[0] *** ", tracks[1].track.album.images);
+        // console.log("    \n\n\n *** tracks[1].track.album.images[0] *** ", tracks[1].track.album.images[2]);
+        // console.log("    \n\n\n    *** tracks[1].track.name *** :         ",tracks[1].track.name);
+        // console.log("    *** tracks[1].track.artists.name *** : ", tracks[1].track.artists[0].name);
+        // console.log("      \n      <<<<<<<<<<  >>>>>>>>>>> \n "); 
       // console.log("      \n      <<<<<<<<<<  >>>>>>>>>>> \n "); 
+        // console.log("      \n      <<<<<<<<<<  >>>>>>>>>>> \n "); 
 
-      console.log("\n\n\n\n\n")
+        console.log("\n\n\n\n\n")
 
-      let playlistInfo = []
+        let playlistInfo = []
 
-      for (let i = 0; i < tracks.length; i++){
-        let addedAt = tracks[i].added_at;
-        let track = tracks[i].track.name;
-        let artist = tracks[i].track.artists[0].name;
-        let explicit = tracks[i].track.explicit;
-        let image_href = tracks[i].track.album.images[2].url;
+        for (let i = 0; i < tracks.length; i++) {
+          let addedAt = tracks[i].added_at;
+          let track = tracks[i].track.name;
+          let artist = tracks[i].track.artists[0].name;
+          let explicit = tracks[i].track.explicit;
+          let image_href = tracks[i].track.album.images[2].url;
 
-        // console.log("    \n    *** addeAt                   :         ",addedAt);
-        // console.log("    *** track                    :         ",track);
-        // console.log("    *** artist                   : ", artist);
-        // console.log("    *** explicit                 : ", explicit);
-        // console.log("    *** tracks[i].track.album.images[2] *** : ", image_href);
+          // console.log("    \n    *** addeAt                   :         ",addedAt);
+          // console.log("    *** track                    :         ",track);
+          // console.log("    *** artist                   : ", artist);
+          // console.log("    *** explicit                 : ", explicit);
+          // console.log("    *** tracks[i].track.album.images[2] *** : ", image_href);
 
-        playlistInfo[i] ={addedAt : addedAt, track: track, artist: artist, explicit: explicit, image_href: image_href}
-        playlistInfo.push(playlistInfo[i]);
+          playlistInfo[i] = { addedAt: addedAt, track: track, artist: artist, explicit: explicit, image_href: image_href }
+          playlistInfo.push(playlistInfo[i]);
+        }
+
+        console.log("  \n\n\n\n\n  playlistInfo", playlistInfo)
+
+        res.send(JSON.stringify(playlistInfo));
       }
-
-      console.log("  \n\n\n\n\n  playlistInfo", playlistInfo)
-      
-      res.send(JSON.stringify(playlistInfo));
-    }
-  )
-});
+      )
+  });
 
   app.get("/spotify/track/:search", function (req, res) {
     spotifyApi.searchTracks(req.params.search)
-        .then(function (data) {
-            res.json(data.body.tracks.items[0]);
-        }, function (err) {
-            console.error(err);
-        });
+      .then(function (data) {
+        res.json(data.body.tracks.items[0]);
+      }, function (err) {
+        console.error(err);
+      });
   });
 
-/*app.get("/spotify/me", function (req, res) {
-
-  spotifyApi.getMe()
-  .then(function(data) {
-    console.log('Some information about the authenticated user', data.body);
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
-});*/
+  /*app.get("/spotify/me", function (req, res) {
+  
+    spotifyApi.getMe()
+    .then(function(data) {
+      console.log('Some information about the authenticated user', data.body);
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+  });*/
 
 };
